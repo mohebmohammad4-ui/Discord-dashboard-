20f5ed4358b78643adbb0f67cf5c36a8d5cbcf61
mohebmohammad4-ui <mohebmohammad4@gmail.com>
2026-05-30 20:16:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const Guild = mongoose.model("GuildConfig", GuildConfigSchema);
 
 // ================== MIDDLEWARE ==================
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "view"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
